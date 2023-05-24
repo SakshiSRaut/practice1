8ed2e2377a0bc3af41a1992059baa5419970257a
sakshi <sakshiraut374@gmail.com>
2023-05-24 15:49:01 +0530

    added one comment

--- ass51.cpp
+++ ass51.cpp
@@ -17,14 +17,7 @@ using namespace std;
 
 //added comment in local area 
 int iNo1;
-int iNo3;
-
-int iNo2;
-
-int iNo5;
-int iNo11;
-int iNo45;
-
+iNo1 =123;
 void get_file_list(string DATA_DIR)
 {
     HANDLE hFind;
